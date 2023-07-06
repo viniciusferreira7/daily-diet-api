b2ae3cdb899babb1419dca37d50bb119837c9ea7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-07-05 21:16:34 -0300

    feat: add table typing in knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      session_id: string
+      name: string
+      created_at: string
+    }
+    meals: {
+      id: string
+      session_id: string
+      name: string
+      description: string
+      is_diet: boolean
+      created_at: string
+    }
+  }
+}
