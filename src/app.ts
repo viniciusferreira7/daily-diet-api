48a5e437f11c62d5c47f73d47b08b9de307e636b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-07-08 18:30:53 -0300

    feat: add the cookie inside the user route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { userRoutes } from './routes/user'
 
 export const app = fastify()
 
+app.register(cookie)
 app.register(userRoutes, {
   prefix: 'user',
 })
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,7 @@
+import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { knex } from '../database'
 
 export async function userRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
@@ -13,6 +15,27 @@ export async function userRoutes(app: FastifyInstance) {
 
     const { name } = createUserBodySchema.parse(request.body)
 
-    return reply.status(201).send()
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 30 days
+      })
+
+      await knex('users').insert({
+        id: randomUUID(),
+        session_id: sessionId,
+        name,
+      })
+
+      return reply.status(201).send()
+    }
+
+    return reply.status(401).send({
+      message: 'You already have a user registered',
+    })
   })
 }
