d12057fdf5b4b73094c3b898cd4a2cb6dce5f9c9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-07-22 17:15:13 -0300

    test: configure the environment for testing

---
+++ test/user.spec.ts
@@ -0,0 +1,17 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+
+describe('User routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
