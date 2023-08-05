54bf7e47e8cfd6071825fca2604d4c0db0de31f3
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-08-05 18:52:46 -0300

    test: takes the total meals outside the diet

--- test/meal.spec.ts
+++ test/meal.spec.ts
@@ -142,5 +142,43 @@ describe('Meal routes', () => {
       }),
     )
   })
+  it.skip('should be able to get total meals outside the diet', async () => {
+    const createUserResponse = await request(app.server).post('/user').send({
+      name: 'Vinicius',
+    })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    await request(app.server).post('/meal').set('Cookie', cookies).send({
+      name: 'Almoço',
+      description: 'Arroz e feijão',
+      isDiet: 'true',
+    })
+
+    await request(app.server).post('/meal').set('Cookie', cookies).send({
+      name: 'Janta',
+      description: 'Arroz e Salada',
+      isDiet: 'true',
+    })
+
+    await request(app.server).post('/meal').set('Cookie', cookies).send({
+      name: 'Sobremesa',
+      description: 'Chocolate',
+      isDiet: 'false',
+    })
+
+    const totalMeals = await request(app.server)
+      .get('/meal/summary/off-diet')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(totalMeals.body).toEqual(
+      expect.objectContaining({
+        message: {
+          total: 1,
+        },
+      }),
+    )
+  })
 })
 // TODO: Remove skip from tests
